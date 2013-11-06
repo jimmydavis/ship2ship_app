7e84a9f0dae3e2e51f0e2602fad729c989728f4c
James Davis <jamesmdavis5@gmail.com>
2013-11-06 09:22:16 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ship2shipApp::Application.config.secret_token = 'c452e2ef92f3e10eaddc7d64868da3daa9ed2e1548764e853b8833258942e6d1e4e75a38542314f453553d46703a7cbae7d7cef6bcd10abd9a835640061db1a9'
